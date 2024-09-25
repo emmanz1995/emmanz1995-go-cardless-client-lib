bb10062d68c495c9e6905a5955a9adab2ed914c7
emmanuel okuchukwu <emmanuelokuchukwu@emmanuels-MacBook-Pro.local>
2024-09-25 22:10:41 +0100

    new changes

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.goCardlessClient = void 0;
+var goCardlessRequest_1 = require("./goCardlessRequest");
+Object.defineProperty(exports, "goCardlessClient", { enumerable: true, get: function () { return __importDefault(goCardlessRequest_1).default; } });
